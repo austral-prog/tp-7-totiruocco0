18513a31d6f33433808eab7dbcf6166ab9a712f0
totiruocco0 <162723302+totiruocco0@users.noreply.github.com>
2024-05-31 09:03:51 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,18 +1,41 @@
-def index_of_by_index(word, list, index):
+def index_of_by_index(word, lista, index):
+
+    lista1= lista[index::]
+    lista2= lista[:index:]
+    lista= lista2+lista1
+    for i,e in enumerate(lista1):
+        if e==word:
+            for o,e in enumerate(lista2):
+                if e==word:
+                    del lista2[o]
+                    lista2.insert(i,"")
+                    lista=lista2+lista1
+    for u,e in enumerate (lista):
+        if e==word and word in lista1:
+            return u
     return -1
-
-
-def index_of_empty(list):
+def index_of_empty(lista):
+    for i,e in enumerate(lista):
+        if e=="":
+            return  i
     return -1
-
-
-def index_of(word, list):
+def index_of(word, lista):
+    for i,e in enumerate(lista):
+        if e==word:
+            return i
     return -1
-
-
-def put(word, list):
+def put(word, lista):
+    for i,e in enumerate(lista):
+        if e=="":
+            del lista[i]
+            lista.insert(i,word)
+            return i
     return -1
-
-
-def remove(word, list):
-    return -1
+def remove(word, lista):
+    nueva=[]
+    for i,e in enumerate(lista):
+        if e==word:
+            del lista[i]
+            lista.insert(i,"")
+            nueva+=[""]
+    return len(nueva)

05dcfced29c54473ab358e72fecf70402edd3782
totiruocco0 <162723302+totiruocco0@users.noreply.github.com>
2024-05-31 09:04:59 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,12 @@
-def enumerate_list(list):
-    return list
+def enumerate_list(lst):
+    return [f"{i}. {word}" for i, word in enumerate([word for word in lst if word != ""])]
+colors = ["Red", "Green", "", "White", "Black"]
 
+print(enumerate_list(colors))
 
-def enumerate_backwards(list):
-    return list
+def enumerate_backwards(lst):
+    return [f"{i}. {word[::-1]}" for i, word in enumerate([word for word in lst if word != ""])]
+
+colors = ["Red", "Green", "", "White", "Black"]
+
+print(enumerate_backwards(colors))
